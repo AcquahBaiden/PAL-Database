92de20e130fbf4bb463a5ec9f7a72559306f8fe2
Acquah Baiden <jbaiden@live.com>
2020-12-30 13:42:09 -0500

    Update environment.ts

--- environments/environment.ts
+++ environments/environment.ts
@@ -4,14 +4,10 @@
 
 export const environment = {
   production: false,
-  firebase:{
-    apiKey: "AIzaSyDW7Vh0bnSAw2I9A3q_FBWWc1fuRnIe8To",
-  authDomain: "pal-database.firebaseapp.com",
-  projectId: "pal-database",
-  storageBucket: "pal-database.appspot.com",
-  messagingSenderId: "629610552721",
-  appId: "1:629610552721:web:ee070d36740df5124b8e30",
-  measurementId: "G-2JYGD89HN8"
+  firebase:{/*
+  Your firebase API credentials here
+  
+  */
   }
 };
 
